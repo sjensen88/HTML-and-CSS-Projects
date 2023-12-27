b02489deeb69ab589f11c9187ea0f88855e8ab97
Stephanie <sjensen88@gmail.com>
2023-12-26 21:05:38 -0600

    Update Basic_JavaScript_1.js
    fixed the indentation

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,6 @@
 function My_First_Function(){
-		var String = “Kiss me, I’m Irish!”;
-		var result = String.fontcolor(“green”);
-		document.getElementById(“Irish”).innerHTML =
-		result;
+	var String = “Kiss me, I’m Irish!”;
+	var result = String.fontcolor(“green”);
+	document.getElementById(“Irish”).innerHTML =
+	result;
 } 

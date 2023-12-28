23a19b44664896ab9917d4d364464cd5c094b992
Stephanie <sjensen88@gmail.com>
2023-12-28 11:04:29 -0600

    Basic_HTML_and_JavaScript_1 Folder Contents

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,5 @@
 function My_First_Function(){
-	var String = “Kiss me, I’m Irish!”;
-	var result = String.fontcolor(“green”);
-	document.getElementById(“Irish”).innerHTML =
-	result;
+	var String = "Kiss me, I'm Irish!";
+	document.getElementById("Irish").innerHTML = String;
+	document.getElementById("Irish").style.color = 'green';
 } 
